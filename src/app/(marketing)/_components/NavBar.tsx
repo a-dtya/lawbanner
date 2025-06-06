e9968ce2331b81b7aafd5e3d497239cca9d1e4df
a-dtya <adithyavinod1943@gmail.com>
2025-06-07 00:23:52 +0530

    added brand-logo, refactored navbar

--- src/app/(marketing)/_components/NavBar.tsx
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
-
+import {BrandLogo} from "../../../components/BrandLogo";
 
 export function NavBar() {
-    return <header>
+    return <header className="flex py-4 shadow-xl fixed top-0 w-full z-50 bg-background/90">
         <nav className="flex items-center gap-10 container font-semibold">
-            <Link href="/" className="mr-auto">Home</Link>
+            <Link href="/" className="mr-auto"><BrandLogo/></Link>
+            <Link href="#" className="text-lg">Features</Link>
+            <Link href="#pricing" className="text-lg">Pricing</Link> 
+            <Link href="#" className="text-lg">About</Link>
         </nav>
     </header>;
 }
---
+++ src/components/BrandLogo.tsx
@@ -0,0 +1,9 @@
+import { EarthLock } from "lucide-react";
+
+
+export function BrandLogo() {
+    return <span className="flex items-center gap-2 font-semibold flex-shrink-0">
+        <EarthLock className="size-8"/>
+        <span className="text-lg">LawBanner</span>
+    </span>
+}
