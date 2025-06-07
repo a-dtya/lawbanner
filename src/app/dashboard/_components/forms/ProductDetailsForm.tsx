fa210ec7d6e466d6384fd59b206e666cf4a14294
a-dtya <adithyavinod1943@gmail.com>
2025-06-08 01:28:08 +0530

    error handling on api fetch fail, or async task

--- src/app/dashboard/_components/forms/ProductDetailsForm.tsx
+++ src/app/dashboard/_components/forms/ProductDetailsForm.tsx
@@ -8,10 +8,15 @@ type ProductDetailFieldTypes = {
 
 export default function ProductDetailsForm(){
     {/*Now we have to register the input fields with the useForm*/}
-    const {register, handleSubmit, formState: {errors}} = useForm<ProductDetailFieldTypes>()
+    const {register, handleSubmit, setError, formState: {errors, isSubmitting}} = useForm<ProductDetailFieldTypes>()
 
     const onSubmit: SubmitHandler<ProductDetailFieldTypes> = (data) => {
-        console.log(data)
+        try{
+            console.log(data)
+        }
+        catch(error){
+            setError("root", {type: "server", message: "Something went wrong while creating the product"})
+        }
     }
 
      {/*Handle submit prevents default behaviour of form submission*/}
@@ -23,22 +28,23 @@ export default function ProductDetailsForm(){
             <div>
                 <label htmlFor="name">Name</label>
                 <input type="text" id="name" {...register("name", {required: "Name is required", minLength: {value: 3, message: "Name must be at least 3 characters long"}})}/>
-                {errors.name && <p>{errors.name.message}</p>}
+                {errors.name && <p className="text-red-500">{errors.name.message}</p>}
             </div>
             {/*URL*/}
             <div>
                 <label htmlFor="url">URL</label>
                 <input type="text" id="url" {...register("url", {required: "URL is required", pattern: {value: /https?:\/\//, message: "URL must start with http or https"}})}/>
-                {errors.url && <p>{errors.url.message}</p>}
+                {errors.url && <p className="text-red-500">{errors.url.message}</p>}
             </div>
             {/*Description*/}
             <div>
                 <label htmlFor="description">Description</label>
                 <textarea id="description" {...register("description", {maxLength: {value: 100, message: "Description must be at most 100 characters long"}})}/>
-                {errors.description && <p>{errors.description.message}</p>}
+                {errors.description && <p className="text-red-500">{errors.description.message}</p>}
             </div>
             {/*Submit button*/}
-            <button type="submit">Submit</button>
+            <button type="submit" disabled={isSubmitting}>{isSubmitting ? "Creating..." : "Create Product"}</button>
+            {errors.root && <p className="text-red-500">{errors.root.message}</p>}
         </form>
     )
 }
