27e2b4f684408ea80ea5cde4c85df107d243a001
a-dtya <adithyavinod1943@gmail.com>
2025-06-07 12:51:38 +0530

    fixed import issue drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "drizzle-kit";
-import { env } from "../data/env/server";
+import { env } from "@/data/env/server";
 
 export default defineConfig({
   dialect: "postgresql",
