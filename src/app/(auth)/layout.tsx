68e14294a9f87b14c075fe0ae3ac74e078ac3170
a-dtya <adithyavinod1943@gmail.com>
2025-06-07 11:29:39 +0530

    added auth routes sign-in and sign-up

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+export default function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <div className="flex flex-col min-h-screen items-center justify-center">{children}</div>;
+}
