ed35a5bbb5222f31c0c261b0fb396174b39850c0
a-dtya <adithyavinod1943@gmail.com>
2025-06-08 01:12:39 +0530

    added basic validation using formState

--- src/app/dashboard/_components/forms/ProductDetailsForm.tsx
+++ src/app/dashboard/_components/forms/ProductDetailsForm.tsx
@@ -8,7 +8,7 @@ type ProductDetailFieldTypes = {
 
 export default function ProductDetailsForm(){
     {/*Now we have to register the input fields with the useForm*/}
-    const {register, handleSubmit} = useForm<ProductDetailFieldTypes>()
+    const {register, handleSubmit, formState: {errors}} = useForm<ProductDetailFieldTypes>()
 
     const onSubmit: SubmitHandler<ProductDetailFieldTypes> = (data) => {
         console.log(data)
@@ -22,17 +22,20 @@ export default function ProductDetailsForm(){
             {/*Name*/}
             <div>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" {...register("name")}/>
+                <input type="text" id="name" {...register("name", {required: "Name is required", minLength: {value: 3, message: "Name must be at least 3 characters long"}})}/>
+                {errors.name && <p>{errors.name.message}</p>}
             </div>
             {/*URL*/}
             <div>
                 <label htmlFor="url">URL</label>
-                <input type="text" id="url" {...register("url")}/>
+                <input type="text" id="url" {...register("url", {required: "URL is required", pattern: {value: /https?:\/\//, message: "URL must start with http or https"}})}/>
+                {errors.url && <p>{errors.url.message}</p>}
             </div>
             {/*Description*/}
             <div>
                 <label htmlFor="description">Description</label>
-                <textarea id="description" {...register("description")}/>
+                <textarea id="description" {...register("description", {maxLength: {value: 100, message: "Description must be at most 100 characters long"}})}/>
+                {errors.description && <p>{errors.description.message}</p>}
             </div>
             {/*Submit button*/}
             <button type="submit">Submit</button>
