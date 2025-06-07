0250bd2041c3f928be577cc16c0cae6a437cd17e
a-dtya <adithyavinod1943@gmail.com>
2025-06-08 01:35:19 +0530

    added zod for easier validation

--- src/app/dashboard/_components/forms/ProductDetailsForm.tsx
+++ src/app/dashboard/_components/forms/ProductDetailsForm.tsx
@@ -1,14 +1,32 @@
 import {useForm, SubmitHandler} from "react-hook-form"
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
 
-type ProductDetailFieldTypes = {
-    name: string,
-    url: string,
-    description: string
-}
+const productDetailsSchema = z.object({
+    name: z.string().min(3, "Name must be at least 3 characters long"),
+    url: z.string().url("Invalid URL"),
+    description: z.string().max(100, "Description must be at most 100 characters long").min(5, "Description must be at least 5 characters long")
+})
+type ProductDetailFieldTypes = z.infer<typeof productDetailsSchema>
+    
+
+// type ProductDetailFieldTypes = {
+//     name: string,
+//     url: string,
+//     description: string
+// }
 
 export default function ProductDetailsForm(){
     {/*Now we have to register the input fields with the useForm*/}
-    const {register, handleSubmit, setError, formState: {errors, isSubmitting}} = useForm<ProductDetailFieldTypes>()
+    {/*We use the zod schema to validate the form. connect hook form to zod schema using resolver*/}
+    const {register, handleSubmit, setError, formState: {errors, isSubmitting}} = useForm<ProductDetailFieldTypes>({
+        resolver: zodResolver(productDetailsSchema),
+        defaultValues:{
+            name: "test website",
+            url: "https://testwebsite.com",
+            description: "This is a test website"
+        }
+    })
 
     const onSubmit: SubmitHandler<ProductDetailFieldTypes> = (data) => {
         try{
@@ -27,19 +45,19 @@ export default function ProductDetailsForm(){
             {/*Name*/}
             <div>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" {...register("name", {required: "Name is required", minLength: {value: 3, message: "Name must be at least 3 characters long"}})}/>
+                <input type="text" id="name" {...register("name")}/>
                 {errors.name && <p className="text-red-500">{errors.name.message}</p>}
             </div>
             {/*URL*/}
             <div>
                 <label htmlFor="url">URL</label>
-                <input type="text" id="url" {...register("url", {required: "URL is required", pattern: {value: /https?:\/\//, message: "URL must start with http or https"}})}/>
+                <input type="text" id="url" {...register("url")}/>
                 {errors.url && <p className="text-red-500">{errors.url.message}</p>}
             </div>
             {/*Description*/}
             <div>
                 <label htmlFor="description">Description</label>
-                <textarea id="description" {...register("description", {maxLength: {value: 100, message: "Description must be at most 100 characters long"}})}/>
+                <textarea id="description" {...register("description")}/>
                 {errors.description && <p className="text-red-500">{errors.description.message}</p>}
             </div>
             {/*Submit button*/}
