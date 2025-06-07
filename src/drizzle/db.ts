3984f723b617288984186ed57b2e7bec00597d80
a-dtya <adithyavinod1943@gmail.com>
2025-06-07 13:08:07 +0530

    init schema.ts with products table

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,7 +1,9 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { env } from '../data/env/server';
 
-const sql = neon(process.env.DATABASE_URL as string);
+// const sql = neon(process.env.DATABASE_URL as string); not type safe, use when t3-oss not installed
+const sql = neon(env.DATABASE_URL);
 const db = drizzle({ client: sql });
 
 
