a79144431192ed423126d1856ba3411b68ad14db
a-dtya <adithyavinod1943@gmail.com>
2025-06-09 00:32:57 +0530

    added dropdown to productcard

--- src/app/dashboard/_components/ProductCard.tsx
+++ src/app/dashboard/_components/ProductCard.tsx
@@ -1,5 +1,8 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { GripHorizontal } from "lucide-react"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu"
 
 export default function ProductCard({
     id,
@@ -15,11 +18,35 @@ export default function ProductCard({
     return(
         <Card>
             <CardHeader>
-                <CardTitle><Link href={`/dashboard/products/${id}/edit`}>{name}</Link></CardTitle>
+                <div className="flex gap-2 justify-between items-end">
+                    <CardTitle><Link href={`/dashboard/products/${id}/edit`}>{name}</Link></CardTitle>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" className="size-8 p-0">
+                                <GripHorizontal className="size-4"/>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem asChild>
+                                <Link href={`/dashboard/products/${id}/edit`}>Edit</Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>
+                                Add to Site
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>
+                                Delete
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>    
+                    </DropdownMenu>
+                </div>
+                <CardDescription>{url}</CardDescription>
             </CardHeader>
             <CardContent>
                 <p className="text-sm text-muted-foreground">{description}</p>
-                <p className="text-sm text-muted-foreground">{url}</p>
             </CardContent>
         </Card>
     )
